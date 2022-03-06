64269d73877470dfd9b1c2ba90a8f8c5f9650f55
Toky Andriamahefa <toky.andriamahefa@ezymob.fr>
2022-03-06 15:23:46 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,68 @@
+import io
+import json
+
+import mysql.connector
+from flask import Flask
+
+from cloudfunction.util import loadJsonFromS3, saveJsonToDB
+from secrets import aws_secret_access_key, aws_access_key_id
+import boto3
+import pandas as pd
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world() :  # put application's code here
+    return 'Hello World!'
+
+@app.route('/saveProfileIG')
+def saveProfileIG():
+    # save
+    # recup
+    # loadindb
+    cnx = mysql.connector.connect(user='admin', password='awsproject',
+                                  host='mysql-project.clgag50aevk1.eu-west-3.rds.amazonaws.com',
+                                  database='project')
+    file_name = "cedric_andriam"
+    response = loadJsonFromS3(file_name)
+    response = json.loads(response)
+    res = saveJsonToDB(response, cnx)
+    return res
+
+@app.route('/S3toRDS')
+def S3toRDS() :
+    cnx = mysql.connector.connect(user='admin', password='awsproject',
+                                  host='mysql-project.clgag50aevk1.eu-west-3.rds.amazonaws.com',
+                                  database='project')
+    client = boto3.client('s3',
+                          aws_access_key_id=aws_access_key_id,
+                          aws_secret_access_key=aws_secret_access_key)
+    s3_object = client.get_object(Bucket='tokyaws', Key='AWS_project.csv')
+    body = s3_object['Body']
+    response = body.read().decode("utf-8")
+    data = io.StringIO(response)
+    df = pd.read_csv(data, sep=";", header=0)
+    print(df.head())
+    print(df.columns)
+    add_matiere = ("INSERT INTO matiere (matiere, description, nbheure) VALUES (%s,%s,%s)")
+    try :
+        cursor = cnx.cursor()
+        print("Insert all from csv")
+        for i in df.index :
+            new_matiere = (df["Matière"][i], df["Description"][i], int(df[" Nb heure"][i]))
+            print(new_matiere)
+            cursor.execute(add_matiere, new_matiere)
+    finally :
+        cnx.commit()
+        cursor.close()
+        cnx.close()
+    return response
+
+
+# @app.route('/RDStoS3')
+# def RDStoS3() :
+
+
+if __name__ == '__main__' :
+    app.run(host='0.0.0.0', port=3000)
---
+++ cloudfunction/util.py
@@ -0,0 +1,74 @@
+import boto3
+
+from secrets import aws_access_key_id, aws_secret_access_key
+
+
+def loadJsonFromS3(file_name) :
+    file_name = file_name + ".json"
+    client = boto3.client('s3',
+                          aws_access_key_id=aws_access_key_id,
+                          aws_secret_access_key=aws_secret_access_key)
+    s3_object = client.get_object(Bucket='tokyaws', Key=file_name)
+    body = s3_object['Body']
+    response = body.read().decode("utf-8")
+    return response
+
+
+def createItemVideo(item) :
+    user_name = item["username"]
+    url = item["display_url"]
+    nb_like = item["edge_media_preview_like"]["count"]
+    nb_view = item["video_view_count"]
+    nb_comment = item["edge_media_to_comment"]["count"]
+    is_video = item["is_video"]
+    nb_tags = len(item["tags"])
+    comments_disabled = item["comments_disabled"]
+    description = item["edge_media_to_caption"]["edges"][0]["node"]["text"]
+    new_video = (user_name, url,comments_disabled,description, nb_like, nb_comment, is_video, nb_tags, nb_view)
+    return new_video
+
+def createItemImage(item) :
+    user_name = item["username"]
+    url = item["display_url"]
+    nb_like = item["edge_media_preview_like"]["count"]
+    nb_view = item["edge_media_preview_like"]["count"]
+    nb_comment = item["edge_media_to_comment"]["count"]
+    is_video = item["is_video"]
+    nb_tags = len(item["tags"])
+    comments_disabled = item["comments_disabled"]
+    description = item["edge_media_to_caption"]["edges"][0]["node"]["text"]
+    new_image = (user_name, url,comments_disabled,description, nb_like, nb_comment, is_video, nb_tags, nb_view)
+    return new_image
+
+
+def saveJsonToDB(data, cnx) :
+    content = data["GraphImages"]
+
+    add_content = ("INSERT INTO instagram_data (user_name, url, comments_is_disabled, description, nb_like, nb_comment, is_video, nb_tags, nb_view)"
+                   " VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)")
+
+    for item in content:
+        if item["is_video"] == True:
+            add_video = createItemVideo(item)
+            try:
+                cursor = cnx.cursor()
+                print("Insert a video")
+                cursor.execute(add_content, add_video)
+            finally :
+                cnx.commit()
+                cursor.close()
+                cnx.close()
+        else:
+            add_image = createItemImage(item)
+            try:
+                cursor = cnx.cursor()
+                print("Insert a video")
+                cursor.execute(add_content, add_image)
+            finally :
+                cnx.commit()
+                cursor.close()
+                cnx.close()
+        break
+
+
+    return "Done"
